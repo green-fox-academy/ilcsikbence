f529f498f204193acd199515260617bdc8131e1f
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-09 23:32:12 +0200

    BinaryToDecimal.java added

--- week-02/day-3/src/Diagonals.java
+++ week-02/day-3/src/Diagonals.java
@@ -7,7 +7,7 @@ public class Diagonals {
     // Draw the canvas' diagonals.
     // If it starts from the upper-left corner it should be green, otherwise it should be red.
 
-    
+
 
     graphics.setColor(Color.GREEN);
     graphics.drawLine(0, 0, WIDTH, HEIGHT);
