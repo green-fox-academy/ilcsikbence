312d22ec49b9f444c5e30ab87148562ae1b168e2
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-14 11:53:05 +0200

    CountLines.java added

---
+++ week-03/day-2/src/CountLines.java
@@ -0,0 +1,24 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+  public static void main(String[] args) {
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file, and
+    // should not raise any error.
+    String fileName = "amy-file.txt";
+    numberOfLines(fileName);
+  }
+  public static void numberOfLines (String fileName) {
+    try {
+      Path filePath = Paths.get("C:/greenfox/ilcsikbence/week-03/day-2/src/" + fileName);
+      List<String> numberLines = Files.readAllLines(filePath);
+      System.out.println(numberLines.size());
+    } catch (Exception exception){
+      System.out.println(0);
+    }
+  }
+}
