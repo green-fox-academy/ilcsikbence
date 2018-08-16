be32c8238ef00c6de25b47d21d77aaa117fd5edb
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-16 17:52:23 +0200

    NumberAdder.java was modified

--- week-03/day-4/NumberAdder/src/NumberAdder.java
+++ week-03/day-4/NumberAdder/src/NumberAdder.java
@@ -1,5 +1,14 @@
 public class NumberAdder {
   public static void main(String[] args) {
-    
+    System.out.println(numberAdder(5));
+  }
+
+  public static int numberAdder (int n) {
+
+    if (n == 0) {
+      return 0;
+    } else {
+      return numberAdder(n - 1) + n;
+    }
   }
 }
