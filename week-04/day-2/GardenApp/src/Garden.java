9036e8a36013e290149e4da1db08d1f786a793b7
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-21 17:45:40 +0200

    GardenApp Garden.java added

---
+++ week-04/day-2/GardenApp/src/Garden.java
@@ -0,0 +1,18 @@
+public class Garden {
+  public static void main(String[] args) {
+
+    Flower red = new Flower("red");
+    Flower yellow = new Flower("yellow");
+    Tree purple = new Tree("purple");
+    Tree orange = new Tree("orange");
+
+
+
+  }
+
+  public static double toWaterAll(double waterAmount) {
+
+
+
+  }
+}
