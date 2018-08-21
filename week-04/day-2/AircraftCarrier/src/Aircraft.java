2f498e5be409fe93cd72f7786f66fa8ba3cad0c3
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-21 17:48:40 +0200

    AircraftCarrier Aircraft.java added

---
+++ week-04/day-2/AircraftCarrier/src/Aircraft.java
@@ -0,0 +1,45 @@
+public class Aircraft {
+  int currentAmmo;
+  int maxAmmo;
+  int baseDamage;
+  int damageAll;
+  String type;
+  boolean fillQueue;
+
+  public Aircraft() {
+  }
+
+  public void fight() {
+    damageAll = this.baseDamage * currentAmmo;
+    currentAmmo = 0;
+  }
+
+  public int refill(int ammoAdded) {
+    int toFill = maxAmmo - currentAmmo;
+
+    if (ammoAdded <= toFill) {
+      currentAmmo = ammoAdded;
+    } else {
+      currentAmmo = maxAmmo;
+    }
+
+    ammoAdded = ammoAdded - toFill;
+    if (ammoAdded > 0) {
+      return ammoAdded;
+    } else {
+      return 0;
+    }
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getStatus() {
+    return "Type " + type + ", Ammo: " + currentAmmo + ", Base Damage: " + baseDamage + ", All Damage: " + damageAll;
+  }
+
+  public boolean isPriority() {
+    return fillQueue;
+  }
+}
