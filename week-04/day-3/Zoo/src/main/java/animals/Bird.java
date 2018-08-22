ae4a168bee69201ef281f1ec525a565e4b6cb729
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-22 16:26:11 +0200

    Zoo: Bird.java added

---
+++ week-04/day-3/Zoo/src/main/java/animals/Bird.java
@@ -0,0 +1,22 @@
+package main.java.animals;
+
+public class Bird extends Animal {
+
+  public Bird(String name) {
+    this.name = name;
+    numOfLegs = 2;
+    canFly = true;
+  }
+
+  public Bird(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    numOfLegs = 2;
+    canFly = true;
+  }
+
+  public String breed() {
+    return "laying eggs.";
+  }
+}
