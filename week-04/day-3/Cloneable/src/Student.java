185b2e73c2d0e77cdf874e325fce8d1c174c5057
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-22 15:05:15 +0200

    Students.java added

---
+++ week-04/day-3/Cloneable/src/Student.java
@@ -0,0 +1,37 @@
+public class Student extends Person implements Cloneable {
+
+  int skippedDays;
+  String previousOrganization;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  public int skipDays(int numberOfDays) {
+    return skippedDays += numberOfDays;
+  }
+
+  public Student clone(Student student) {
+    return new Student(this.name, this.age, this.gender, this.previousOrganization);
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + this.age + " year old " + this.gender + " from " + this.previousOrganization + " who skipped " + this.skippedDays + " days from the course already.");
+  }
+}
