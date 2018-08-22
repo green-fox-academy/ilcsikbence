c3b4bf26309a726ed9bc2d67ae1b48628fab2447
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-08-22 16:25:37 +0200

    Zoo: Animal.java added

---
+++ week-04/day-3/Zoo/src/main/java/animals/Animal.java
@@ -0,0 +1,29 @@
+package main.java.animals;
+
+public abstract class Animal {
+  String name;
+  int age;
+  String gender;
+  int numOfLegs;
+  boolean canFly;
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public int getNumOfLegs() {
+    return numOfLegs;
+  }
+
+  public boolean canFly() {
+    return canFly;
+  }
+}
