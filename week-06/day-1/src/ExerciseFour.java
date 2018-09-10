7090243448320b789a9c51e99aeb5500c15f79a0
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-09-10 13:14:22 +0200

    ExerciseFour.java added

---
+++ week-06/day-1/src/ExerciseFour.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ExerciseFour {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+    numbers.stream().filter(x -> x * x > 20).forEach(System.out::println);
+  }
+}
