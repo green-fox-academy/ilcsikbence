fcae0d79bfa7bb2fb15870a205272612f90f0455
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-09-18 16:23:47 +0200

    BankOfSimba: BankAccountServiceImpl: added whitespace

--- week-07/day-2/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/service/BankAccountServiceImpl.java
+++ week-07/day-2/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/service/BankAccountServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class BankAccountServiceImpl implements BankAccountService{
+public class BankAccountServiceImpl implements BankAccountService {
 
   BankAccountRepository bankAccountRepository;
 
