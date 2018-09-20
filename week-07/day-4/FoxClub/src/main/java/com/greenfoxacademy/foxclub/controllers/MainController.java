c5ea1e460e43d0cb74dee1c549381da6b43acca0
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-09-20 10:18:12 +0200

    Fox Club: login page added

--- week-07/day-4/FoxClub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-4/FoxClub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -2,12 +2,18 @@ package com.greenfoxacademy.foxclub.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
   @GetMapping(value = "/")
   public String getMainPage() {
-    return "index.html";
+    return "index";
+  }
+
+  @GetMapping
+  public String getLoginPage() {
+    return "login";
   }
 }
