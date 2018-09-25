e3d84caad08e6cedcf0d6334fafcee4d3fc01f22
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-09-25 13:28:23 +0200

    ConnectionWithMySQL added

---
+++ week-08/day-2/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
@@ -0,0 +1,26 @@
+package com.greenfoxacademy.connectionwithmysql;
+
+import com.greenfoxacademy.connectionwithmysql.models.Todo;
+import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionwithmysqlApplication implements CommandLineRunner {
+
+  TodoRepository todoRepository;
+
+  public ConnectionwithmysqlApplication(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConnectionwithmysqlApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    todoRepository.save(new Todo("This is a test todo line"));
+  }
+}
