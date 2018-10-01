c5c2191edf09a952dcaddc7a0d66f39d9aeb29f9
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-10-01 09:53:32 +0200

    REST project: GET / endpoint added

--- week-09/day-1/REST/src/main/java/com/greenfoxacademy/rest/modells/MainController.java
+++ week-09/day-1/REST/src/main/java/com/greenfoxacademy/rest/modells/MainController.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.rest.modells;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+  @GetMapping (value = "/")
+  public String getIndexPage() {
+    return "index.html";
+  }
 }
