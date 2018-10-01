b5e87658417c10a7c48872d9d2cdbb77b9afcf1f
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-10-01 12:22:57 +0200

    REST project: WelcomeMessage was modified, AppendA.java added

--- week-09/day-1/REST/src/main/java/com/greenfoxacademy/rest/controllers/RestApiController.java
+++ week-09/day-1/REST/src/main/java/com/greenfoxacademy/rest/controllers/RestApiController.java
@@ -2,8 +2,9 @@ package com.greenfoxacademy.rest.controllers;
 
 import com.greenfoxacademy.rest.modells.Doubling;
 import com.greenfoxacademy.rest.modells.Error;
-import com.greenfoxacademy.rest.modells.Greeter;
+import com.greenfoxacademy.rest.modells.WelcomeMessage;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +23,14 @@ public class RestApiController {
   @GetMapping(value = "/greeter")
   public Object getGreeting(@RequestParam(value = "name", required = false) String name,
                             @RequestParam(value = "title", required = false) String title) {
-    if (name.isEmpty() || title.isEmpty()) {
+    if (name == null || title == null) {
       return new Error("Please provide a name!");
     }
+    return new WelcomeMessage(name, title);
+  }
 
-    return new Greeter(name, title);
+  @GetMapping(value = "/appenda/{appendable}")
+  public Object getAppendA(@PathVariable(value = "appendable") String appendable) {
 
   }
 
--- week-09/day-1/REST/src/main/java/com/greenfoxacademy/rest/modells/WelcomeMessage.java
+++ week-09/day-1/REST/src/main/java/com/greenfoxacademy/rest/modells/WelcomeMessage.java
@@ -1,6 +1,20 @@
 package com.greenfoxacademy.rest.modells;
 
 public class WelcomeMessage {
+  private String welcome_message;
+
+  public WelcomeMessage(String name, String title) {
+    welcome_message = "Oh, hi there " + name + ", my " + title + "!";
+  }
+
   public WelcomeMessage() {
+  }
+
+  public String getWelcome_message() {
+    return welcome_message;
+  }
+
+  public void setWelcome_message(String welcome_message) {
+    this.welcome_message = welcome_message;
   }
 }
