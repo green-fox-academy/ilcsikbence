a37e2aa6e6bb6b0cc0730a98e910a4105f09273a
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-10-29 15:12:42 +0100

    Sum extended

--- week-02/day-1/src/Sum.java
+++ week-02/day-1/src/Sum.java
@@ -4,21 +4,18 @@ public class Sum {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
 
-    int number1 = scanner.nextInt();
-
-    System.out.println(sum(number1));
-
+    System.out.println(sum(number));
   }
 
-  public static int sum(int number1) {
-
+  public static int sum(int number) {
     int sum = 0;
-
-    for (int i = 1; i <= number1; ++i) {
+    for (int i = 1; i <= number; ++i) {
       sum += i;
     }
 
     return sum;
   }
+
 }
