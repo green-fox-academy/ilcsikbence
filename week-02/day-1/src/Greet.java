6d47d7377c931e7108325236a30daa9aa15067a5
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-10-29 15:07:01 +0100

    reformatted Greet

--- week-02/day-1/src/Greet.java
+++ week-02/day-1/src/Greet.java
@@ -1,10 +1,7 @@
 public class Greet {
   public static void main(String[] args) {
-
     String al = "Greenfox";
-
     System.out.println(greet(al));
-
   }
 
   public static String greet(String al) {
