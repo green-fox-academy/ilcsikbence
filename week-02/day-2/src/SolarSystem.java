ed9675de94b5789340736559a5df2fe1a96d493e
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-10-29 15:53:56 +0100

    reformatted SolarSystem

--- week-02/day-2/src/SolarSystem.java
+++ week-02/day-2/src/SolarSystem.java
@@ -3,20 +3,13 @@ import java.util.*;
 public class SolarSystem{
   public static void main(String... args){
     ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
-
     int index = 5;
-
     System.out.println(putSaturn(planetList, index));
-
   }
 
   public static ArrayList putSaturn(ArrayList planetList, int index){
-
     planetList.add(index, "Saturn");
-
     return planetList;
-
   }
-
 
 }
