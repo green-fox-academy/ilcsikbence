cf7287fe7e2e4869ae90e9dc3179c9e5d867712c
Bence Ilcsik <ilcsikbence1@gmail.com>
2018-10-29 15:46:15 +0100

    reformatted IsInList

--- week-02/day-2/src/IsInList.java
+++ week-02/day-2/src/IsInList.java
@@ -7,17 +7,13 @@ public class IsInList{
     // it should return "true" if it contains all, otherwise "false"
 
     ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
-
     ArrayList<Integer> containList = new ArrayList<>(Arrays.asList(4, 8, 12, 16));
-
     checkNums(list, containList);
 
   }
 
   public static void checkNums(ArrayList list, ArrayList containList){
-
     boolean booleanList = list.containsAll(containList);
-
     System.out.println(booleanList);
 
   }
