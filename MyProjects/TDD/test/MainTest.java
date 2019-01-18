bc07d9a63003d4ff8bb2a2179234ed38eb0c9de1
Bence Ilcsik <ilcsikbence1@gmail.com>
2019-01-18 10:10:40 +0100

    Test case added: testSumOfNumbers()

--- MyProjects/TDD/test/MainTest.java
+++ MyProjects/TDD/test/MainTest.java
@@ -1,5 +1,17 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
+
+  @Test
+  public void testSumOfNumbers() {
+    int a = 2;
+    int b = 3;
+
+    int expectedResult = 5;
+
+    assertEquals(expectedResult, Main.sumTheseNumbers(a, b));
+  }
 
 }
