f08b9196b3752a9054978be89deb6e30bc621a22
Bence Ilcsik <ilcsikbence1@gmail.com>
2019-01-18 10:06:38 +0100

    TDD testing project setup

--- MyProjects/AircraftCarrierSOLID/src/com/greenfoxacademy/Main.java
+++ MyProjects/AircraftCarrierSOLID/src/com/greenfoxacademy/Main.java
@@ -1,14 +1,10 @@
+package com.greenfoxacademy;
+
 public class Main {
   public static void main(String[] args) {
-    Carrier carrier = new Carrier(10000, 1000);
-    Carrier carrier2 = new Carrier(10000, 1000);
+    Fightable carrier = new Carrier(10000, 1000);
+    Fightable carrier2 = new Carrier(10000, 1000);
+    Fightable camp = new Camp(20000);
 
-    FighterJet F16 = new F16();
-
-    carrier.addMovable(F16);
-
-    carrier.reloadAll();
-    carrier.fight(carrier2);
-    carrier.readStatus();
   }
 }
