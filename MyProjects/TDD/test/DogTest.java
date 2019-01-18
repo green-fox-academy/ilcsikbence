0a5069337acc1f2991b0c894027a0f84be845046
Bence Ilcsik <ilcsikbence1@gmail.com>
2019-01-18 10:28:31 +0100

    DogTest test setup

--- MyProjects/TDD/test/DogTest.java
+++ MyProjects/TDD/test/DogTest.java
@@ -1,5 +1,21 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DogTest {
+
+  Dog dog;
+
+  @Before
+  public void init() {
+    dog = new Dog("Fluffy");
+  }
+
+  @After
+  public void destroy() {
+    dog = null;
+  }
 
 }
